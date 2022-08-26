be0eec6e23593c2a32ef17a9d701e740df8c8d90
NicoStella <nicolas.stella@interatica.com>
2022-08-26 08:36:09 -0300

    tag upload

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,26 @@
 
 export default () => ({
     isLoading: true,
-    entries:[],
+    entries:[
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Phasellus ipsum mi, mollis lacinia feugiat ut, accumsan quis felis. Nunc luctus nec lectus eu rhoncus. In nec lorem porta, vehicula diam id, imperdiet nunc. Vivamus vulputate tortor nec eleifend sodales. In hac habitasse platea dictumst. Etiam a semper mi, quis iaculis lacus. Sed pretium hendrerit enim sit amet commodo.',
+            picture: null
+        },
+        {
+            id: new Date().getTime()+50,
+            date: new Date().toDateString(),
+            text: 'Morbi nisi quam, dapibus vel massa tempor, fermentum facilisis odio. Aliquam dignissim vehicula eros, eu venenatis eros pretium a. Ut eget libero facilisis, molestie leo non, aliquet quam. Suspendisse ornare felis at ipsum luctus luctus. Phasellus eget sagittis est, ac egestas est. Aliquam et pulvinar metus. Nulla lectus magna, faucibus eget diam in, bibendum commodo erat. Pellentesque cursus odio non risus cursus tincidunt. Duis ut leo velit. Sed nec lectus sed felis mollis tincidunt. Integer in purus malesuada, congue arcu vel, sollicitudin nibh. Ut finibus, nunc et facilisis tristique, lacus leo eleifend risus, mattis imperdiet sem risus convallis orci. Nam facilisis convallis erat, sit amet egestas leo pulvinar nec.',
+            picture: null
+        },
+        {
+            id: new Date().getTime()+100,
+            date: new Date().toDateString(),
+            text: 'Etiam id nunc sit amet nibh bibendum finibus id sed urna. Duis pulvinar efficitur lectus dapibus efficitur. Nulla sodales justo nulla, sit amet facilisis lacus imperdiet eget. Suspendisse in augue sit amet sapien fringilla placerat. In quis nulla nec ex sollicitudin aliquet et pulvinar sapien. Phasellus blandit fermentum augue, ut mattis lorem tempor ut. Phasellus sed augue quam. Nam ac consectetur neque. Proin ac feugiat lacus. Nam condimentum, tellus in iaculis rutrum, libero lorem sagittis turpis, at pulvinar ante urna eu lorem. Proin ac venenatis risus, eget bibendum diam. Cras et sem in augue commodo hendrerit.',
+            picture: null
+        },
+    ],
 })
+
+
